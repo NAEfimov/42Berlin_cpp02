0323e07a5836287d34fbd0dcfe9545811968eeed
nefimov <nefimov@student.42berlin.de>
2025-10-23 15:02:18 +0200

    Fixed.cpp: add Fixed class implementetion

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,41 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 18:31:19 by nefimov           #+#    #+#             */
-/*   Updated: 2025/10/21 18:31:20 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/10/23 14:58:23 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : value(0) {
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+	std::cout << "Copy Constructor called" << std::endl;
+	// this->value = other.getRawBits();
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy Assignment Operator called" << std::endl;
+	if (this == &other)
+		return (*this);
+	this->value = other.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value);
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	value = raw;
+}
